945463160555ed1fd46f72402d32f80b10c4d5cb
Kamil Adam <kamil.adam.zabinski@gmail.com>
2025-08-19 18:43:07 +0200

    Add test configuration

---
+++ hs/test/Main.hs
@@ -0,0 +1,9 @@
+module Main where
+
+import qualified Spec
+import           Test.Hspec (hspec)
+import           Test.Hspec.Slow (configure, timeThese)
+
+
+main :: IO ()
+main = (hspec . flip timeThese Spec.spec) =<< configure 1
