1edf01cf564919709ca0e47cb27bccd32d0d8b9d
Kamil Adam <kamil.adam.zabinski@gmail.com>
2025-08-19 20:17:37 +0200

    Use taste

--- hs/test/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import qualified Spec
-import           Test.Hspec (hspec)
-import           Test.Hspec.Slow (configure, timeThese)
-
-
-main :: IO ()
-main = (hspec . flip timeThese Spec.spec) =<< configure 1
